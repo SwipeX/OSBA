f82e9f9ca3227230f5ceadbdd7a4d26f49e9b819
Caleb <me_at_home5@hotmail.com>
2013-09-22 23:27:09 +0100

    Node.next/previous

--- src/org/hexbot/updater/transform/Node.java
+++ src/org/hexbot/updater/transform/Node.java
@@ -1,7 +1,12 @@
 package org.hexbot.updater.transform;
+
 import org.hexbot.updater.Updater;
+import org.hexbot.updater.search.EntryPattern;
+import org.hexbot.updater.search.InsnEntry;
 import org.hexbot.updater.transform.parent.Container;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldInsnNode;
 
 import java.util.Map;
 
@@ -13,7 +18,7 @@ public class Node extends Container {
 
 	@Override
 	public int getTotalHookCount() {
-		return 0;
+		return 2;
 	}
 
 	@Override
@@ -31,5 +36,35 @@ public class Node extends Container {
 
 	@Override
 	public void transform(ClassNode cn) {
+		/**
+		 * next & previous are found in the remove() method
+		 *
+		 * 	<Code>
+		 *
+		 *	if(this.next != null) {
+		 *		this.next.previous = this.previous;
+		 *		this.previous.next = this.next;
+		 *		this.previous = null;
+		 *		this.next = null;
+		 *	}
+		 *
+		 */
+		FieldInsnNode fin;
+		EntryPattern next = new EntryPattern(
+				new InsnEntry(Opcodes.GETFIELD),
+				new InsnEntry(Opcodes.IFNONNULL)
+		);
+		next.find(cn);
+		fin = (FieldInsnNode) next.get(0).getInstance();
+		String desc = "L" + GETTER + ";";
+		addHook("getNext", fin.name, fin.owner, fin.owner, desc, -1);
+		EntryPattern previous = new EntryPattern(
+				new InsnEntry(Opcodes.GETFIELD),
+				new InsnEntry(Opcodes.PUTFIELD)
+		);
+		previous.find(cn);
+		fin = (FieldInsnNode) previous.get(1).getInstance();
+		addHook("getPrevious", fin.name, fin.owner, fin.owner, desc, -1);
 	}
+
 }
