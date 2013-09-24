e6f957539a7b5ddb99141b1a5d4e167283c8c7b1
ExoCode <tylerTsedlar@gmail.com>
2013-09-23 18:54:02 -0700

    ItemLayer (Need to do height)

--- src/org/hexbot/updater/transform/ItemLayer.java
+++ src/org/hexbot/updater/transform/ItemLayer.java
@@ -1,8 +1,12 @@
 package org.hexbot.updater.transform;
 
 import org.hexbot.updater.Updater;
+import org.hexbot.updater.search.EntryPattern;
+import org.hexbot.updater.search.InsnEntry;
 import org.hexbot.updater.transform.parent.Container;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldInsnNode;
 
 import java.util.Map;
 
@@ -14,7 +18,7 @@ public class ItemLayer extends Container {
 
 	@Override
 	public int getTotalHookCount() {
-		return 0;
+		return 5;
 	}
 
 	@Override
@@ -32,5 +36,25 @@ public class ItemLayer extends Container {
 
 	@Override
 	public void transform(ClassNode cn) {
+		ClassNode region = updater.classnodes.get(CLASS_MATCHES.get("Region"));
+		if (region != null) {
+			String render = "L" + CLASS_MATCHES.get("Renderable") + ";";
+			EntryPattern pattern = new EntryPattern(new InsnEntry(Opcodes.PUTFIELD, render, cn.name),
+					new InsnEntry(Opcodes.PUTFIELD, "I", cn.name), new InsnEntry(Opcodes.PUTFIELD, "I", cn.name),
+					new InsnEntry(Opcodes.PUTFIELD, "I", cn.name), new InsnEntry(Opcodes.PUTFIELD, render, cn.name),
+					new InsnEntry(Opcodes.PUTFIELD, render, cn.name));
+			if (pattern.find(region)) {
+				FieldInsnNode item1 = (FieldInsnNode) pattern.get(0).getInstance();
+				FieldInsnNode x = (FieldInsnNode) pattern.get(1).getInstance();
+				FieldInsnNode y = (FieldInsnNode) pattern.get(3).getInstance();
+				FieldInsnNode item2 = (FieldInsnNode) pattern.get(4).getInstance();
+				FieldInsnNode item3 = (FieldInsnNode) pattern.get(5).getInstance();
+				addHook("getItem1", item1.name, item1.owner, cn.name, item1.desc, -1);
+				addHook("getX", x.name, x.owner, cn.name, x.desc, -1);
+				addHook("getY", y.name, y.owner, cn.name, y.desc, -1);
+				addHook("getItem2", item2.name, item2.owner, cn.name, item2.desc, -1);
+				addHook("getItem3", item3.name, item3.owner, cn.name, item3.desc, -1);
+			}
+		}
 	}
 }
