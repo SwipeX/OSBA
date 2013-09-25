0adf9a58f7c5cfb21ce72e0aa1cfdb5c46c0fd67
ExoCode <tylerTsedlar@gmail.com>
2013-09-24 20:37:31 -0700

    Friend

--- src/org/hexbot/updater/transform/Friend.java
+++ src/org/hexbot/updater/transform/Friend.java
@@ -1,8 +1,12 @@
 package org.hexbot.updater.transform;
 
 import org.hexbot.updater.Updater;
+import org.hexbot.updater.search.EntryPattern;
+import org.hexbot.updater.search.InsnEntry;
 import org.hexbot.updater.transform.parent.Container;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldInsnNode;
 
 import java.util.Map;
 
@@ -14,7 +18,7 @@ public class Friend extends Container {
 
 	@Override
 	public int getTotalHookCount() {
-		return 0;
+		return 3;
 	}
 
 	@Override
@@ -33,9 +37,18 @@ public class Friend extends Container {
 
 	@Override
 	public void transform(ClassNode cn) {
-		/**
-		 * We Have No Friends
-		 */
+		ClassNode client = updater.classnodes.get("client");
+		EntryPattern ep = new EntryPattern(new InsnEntry(Opcodes.PUTFIELD, "Ljava/lang/String;", cn.name),
+				new InsnEntry(Opcodes.PUTFIELD, "Ljava/lang/String;", cn.name),
+				new InsnEntry(Opcodes.PUTFIELD, "I", cn.name));
+		if (ep.find(client)) {
+			FieldInsnNode name = (FieldInsnNode) ep.get(0).getInstance();
+			FieldInsnNode prev = (FieldInsnNode) ep.get(1).getInstance();
+			FieldInsnNode world = (FieldInsnNode) ep.get(2).getInstance();
+			addHook("getName", name.name, name.owner, cn.name, name.desc, -1);
+			addHook("getPreviousName", prev.name, prev.owner, cn.name, prev.desc, -1);
+			addHook("getWorld", world.name, world.owner, cn.name, world.desc, -1);
+		}
 	}
 
 }
