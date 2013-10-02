cf435ba228e696324dd0a313beebd9c3c4f27fb1
Swipe <timdeka@gmail.com>
2013-10-02 15:45:36 -0400

    fixed definition searcher

--- src/org/hexbot/updater/transform/ItemDefinition.java
+++ src/org/hexbot/updater/transform/ItemDefinition.java
@@ -36,7 +36,7 @@ public class ItemDefinition extends Container {
     @Override
     public void transform(ClassNode cn) {
         EntryPattern ep = new EntryPattern(new InsnEntry(Opcodes.GETSTATIC, "Ljava/lang/String;"), new InsnEntry(Opcodes.PUTFIELD, "Ljava/lang/String;")
-                ,new InsnEntry(Opcodes.PUTFIELD, "[Ljava/lang/String;"),new InsnEntry(Opcodes.PUTFIELD, "[Ljava/lang/String;"));
+                , new InsnEntry(Opcodes.PUTFIELD, "[Ljava/lang/String;"), new InsnEntry(Opcodes.PUTFIELD, "[Ljava/lang/String;"));
         if (ep.find(cn)) {
             FieldInsnNode name = ep.get(1, FieldInsnNode.class);
             addHook("getName", name.name, name.owner, name.owner, name.desc, -1);
@@ -45,28 +45,26 @@ public class ItemDefinition extends Container {
             FieldInsnNode groundActions = ep.get(3, FieldInsnNode.class);
             addHook("getGroundActions", groundActions.name, groundActions.owner, groundActions.owner, groundActions.desc, -1);
         }
-	    FieldNode members = cn.getField(null, "Z");
-	    addHook("isMembers", members.name, cn.name, cn.name, members.desc, -1);
-		EntryPattern ep1 = new EntryPattern(new InsnEntry(Opcodes.ACONST_NULL),
-				new InsnEntry(Opcodes.GETFIELD, "[I", cn.name));
-	    if (ep1.find(cn)) {
-		    FieldInsnNode ids = (FieldInsnNode) ep1.get(1).getInstance();
-		    addHook("getIds", ids.name, ids.owner, cn.name, ids.desc, -1);
-	    }
-	    EntryPattern ep2 = new EntryPattern(new InsnEntry(Opcodes.ILOAD), new InsnEntry(Opcodes.ALOAD),
-			    new InsnEntry(Opcodes.GETFIELD, "[I", cn.name), new InsnEntry(Opcodes.GETFIELD, "[I", cn.name));
-	    if (ep2.find(cn)) {
-		    FieldInsnNode stacks = (FieldInsnNode) ep2.get(2).getInstance();
-		    addHook("getStacks", stacks.name, stacks.owner, cn.name, stacks.desc, -1);
-	    }
-	    searcher: for (ClassNode c : updater.classnodes.values()) {
-		    for (MethodNode mn : c.methods) {
-			    if ((mn.access & Opcodes.ACC_STATIC) != Opcodes.ACC_STATIC) continue;
-			    if (mn.desc.equals("(II)L" + cn.name + ";")) {
-				    addHook("getItemDefinition*", mn.name, c.name, c.name, "method", -1);
-				    break searcher;
-			    }
-		    }
-	    }
+        FieldNode members = cn.getField(null, "Z");
+        addHook("isMembers", members.name, cn.name, cn.name, members.desc, -1);
+        EntryPattern ep1 = new EntryPattern(new InsnEntry(Opcodes.ACONST_NULL),
+                new InsnEntry(Opcodes.GETFIELD, "[I", cn.name));
+        if (ep1.find(cn)) {
+            FieldInsnNode ids = (FieldInsnNode) ep1.get(1).getInstance();
+            addHook("getIds", ids.name, ids.owner, cn.name, ids.desc, -1);
+        }
+        EntryPattern ep2 = new EntryPattern(new InsnEntry(Opcodes.ILOAD), new InsnEntry(Opcodes.ALOAD),
+                new InsnEntry(Opcodes.GETFIELD, "[I", cn.name), new InsnEntry(Opcodes.GETFIELD, "[I", cn.name));
+        if (ep2.find(cn)) {
+            FieldInsnNode stacks = (FieldInsnNode) ep2.get(2).getInstance();
+            addHook("getStacks", stacks.name, stacks.owner, cn.name, stacks.desc, -1);
+        }
+        for (MethodNode mn : updater.classnodes.get(CLASS_MATCHES.get("ItemDefinition")).methods) {
+            if ((mn.access & Opcodes.ACC_STATIC) != Opcodes.ACC_STATIC) continue;
+            if (mn.desc.equals("(I)L" + cn.name + ";")) {
+                addHook("getItemDefinition*", mn.name, CLASS_MATCHES.get("ItemDefinition"), CLASS_MATCHES.get("ItemDefinition"), "method", -1);
+                break;
+            }
+        }
     }
 }
